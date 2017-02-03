f77af0bc19a9b791b6bdd47d7a6f91b305260f8c
omerk <omerk@jfrog.com>
2017-02-03 22:57:52 +0200

    final

--- facebot-exec/Processing.py
+++ facebot-exec/Processing.py
@@ -4,6 +4,7 @@ import SyncGlobals
 
 running = True
 
+# Confirm passed points are corners of a large enough rectangle
 def isValidRect(paperPoints):
     for i in range (0,4):
         for j in range (i + 1,4):
@@ -11,6 +12,7 @@ def isValidRect(paperPoints):
                 return False
     return True
 
+# Find the center point of the passed rectangle corners
 def centerOfRect(paperPoints):
     sumX = 0
     sumY = 0
@@ -19,49 +21,61 @@ def centerOfRect(paperPoints):
         sumY += paperPoints[i][1]
     return (sumX / 4, sumY  / 4)
 
-
+# Check if passed rectangles are in sufficiently different positions by comparing the center points
 def isDifferentRectangle(lastPaperPoints, paperPoints):
     centerOfLast = centerOfRect(lastPaperPoints)
     centerOfThis = centerOfRect(paperPoints)
     return DetectBlackPixels.linearDistance(centerOfLast, centerOfThis) > 200
 
+# Process current frame and cache data in thread-safe globals
 def processFrame():
     camFrame = SyncGlobals.getCamFrame()
 
     if camFrame is not None:
+        # init variables
         lastPaperPoints = SyncGlobals.getPaperPoints()
         newPaperPoints = PaperHomography.getPaperPoints(camFrame)
         pointAsTuple = None
         paperPoints = None
 
+        # a valid rectangle was recognized in the current frame
         if newPaperPoints is not None and isValidRect(newPaperPoints):
             # apply the four point transform to obtain a top-down
             # view of the original image
             if lastPaperPoints is not None:
                 if not isDifferentRectangle(lastPaperPoints, newPaperPoints):
+                    # cache and work with new paper position
                     SyncGlobals.setPaperPoints(newPaperPoints)
                     paperPoints = newPaperPoints
                 else:
+                    # work with last paper position from cache
                     paperPoints = lastPaperPoints
             else:
+                # on first capture, work with new paper position
                 paperPoints = newPaperPoints
+
             pointAsTuple = PaperHomography.arrayToTuple(paperPoints)
 
+        # a valid rectangle was not recognized, but one is already in cache
         elif lastPaperPoints is not None:
             pointAsTuple = PaperHomography.arrayToTuple(lastPaperPoints)
 
+        # valid rectangle exists
         if pointAsTuple is not None:
             SyncGlobals.setPointAsTuple(pointAsTuple)
 
+            # compute paper sizes
             paperSize = PaperHomography.paperSizer(sorted(pointAsTuple))
             paperSizes = (paperSize[1], paperSize[2])
             SyncGlobals.setPaperSizes(paperSizes)
 
+        # valid rectangle exists
         if paperPoints is not None:
+            # cache current paper as full frame
             scannedInk = PaperHomography.scanInkFromImage(camFrame, paperPoints)
             SyncGlobals.setScannedInk(scannedInk)
 
-
+# Keep processing frames until shutdown
 def processingLoop():
     while running:
         processFrame()
